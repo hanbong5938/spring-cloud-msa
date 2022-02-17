b144fbbb29146b40e128d54979958fcf8435d560
hbbae <hbbae@wecruitcorp.com>
2022-02-17 11:33:26 +0900

    Changes

---
+++ gateway-server/src/test/kotlin/xyz/gorani/gateway/GatewayServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.gorani.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
